1685a7189d86610f8177ba001bb1cdac19cc0e5d
Muhammed Rihan <muhammedrihann16@gmail.com>
2025-01-06 06:02:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,8 +20,22 @@ app.use(express.static("public"));
 
 
 app.get("/",(req, res) => {
+    res.render("landing.ejs");
+});
+
+app.get("/hostel", (req,res) => {
     res.render("index.ejs");
 });
+
+app.get("/login", (req, res) => {
+    res.render("login.ejs"); // Replace with the desired URL
+});
+
+app.get('/dashboard', (req, res) => {
+    res.render('dashboard.ejs');
+
+});
+
 
 db.connect();
 
@@ -29,13 +43,13 @@ app.post("/submit-feedback", (req, res) => {
     const feedback = req.body;
     console.log(feedback);
     
-    db.query("INSERT INTO feedback (name, mess_number, feedback, meal_course) VALUES ($1, $2, $3, $4)",
-      [feedback.name, feedback.mess_number, feedback.comment, feedback.meal_course], (err) => {
+    db.query("INSERT INTO feedback (name, mess_number, feedback, meal_course, rating) VALUES ($1, $2, $3, $4)",
+      [feedback.name, feedback.mess_number, feedback.comment, feedback.meal_course, feedback.rating], (err) => {
         if (err) {
             console.log("Error executing query", err.stack);
             res.status(500).send("Error submitting feedback."); // Send an error response if the query fails
         } else {
-            res.redirect("/"); // Redirect to the home page on successful insertion
+            res.redirect("/hostel"); // Redirect to the home page on successful insertion
         }
         // Do not close the database connection here (db.end()) if you are using connection pooling.
     });
